eec8df06a773831cb6ce57e295bffab0771d8339
Mateus Vilione <mateusvilione@gmail.com>
2020-03-24 19:22:31 -0300

    adição do knex

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.HOST_DB,
+      database: process.env.DATABASE,
+      user:     process.env.USER_DB,
+      password: process.env.PASSWORD_DB
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
