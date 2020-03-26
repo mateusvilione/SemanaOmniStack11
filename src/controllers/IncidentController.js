89054df83dfec043fc8a95bfe08a4638923e4713
Mateus Vilione <mateusvilione@gmail.com>
2020-03-26 16:43:43 -0300

    Adicionando mais informações da ong no json

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -14,9 +14,17 @@ module.exports = {
         response.header('X-Total-Count', count_pages);
 
         const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1 )* 5)
-            .select('*');
+            .select([
+                'incidents.*', 
+                'ongs.name', 
+                'ongs.email', 
+                'ongs.whatsapp', 
+                'ongs.city', 
+                'ongs.uf'
+            ]);
     
         return response.json(incidents);
     },
